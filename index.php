bd99faa3440e378f7b00c7e797660dd197179945
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-01-24 14:32:10 +0200

    finish HomePage

--- index.php
+++ index.php
@@ -1,19 +1,66 @@
-
 <?php include 'header.php'; ?>
 
 
 <!DOCTYPE html>
 <html lang="ro">
+
 <head>
     <meta charset="UTF-8">
     <title>BeniStore</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<h1>Bine ati venit!</h1>
+    <div class="home-content">
+        <h1 class="title-page">Bine ai venit la BeniStore - Expertii in Contructii</h1>
+        <div class="description-company">
+            <div class="img-cotainer">
+                <img src="images/img1.jpg" alt="" class='img'>
+            </div>
+            <p class="paragraf1">Bine ați venit la [Numele Firme], unde ne mândrim cu o vastă experiență în
+                domeniul construcțiilor. Suntem o echipă dedicată, pusă pe punerea în
+                aplicare a fiecărui proiect cu pasiune și excelență. În fiecare cărămidă
+                pe care o așezăm și în fiecare detaliu pe care îl integrăm, descoperiți
+                angajamentul nostru ferm pentru calitate, profesionalism și inovație. Fie
+                că este vorba despre construirea unui cămin, a unui spațiu comercial sau
+                a unei structuri industriale, [Numele Firmei] aduce laolaltă cunoștințe
+                tehnice solide și abordări inovatoare pentru a transforma visurile
+                clienților în realitate durabilă.</p>
+        </div>
 
-
-   
+    </div>
+    <div class="partner-banner">
+        <h4>Parteneri nostri de baza</h4>
+        <div class="partner">
+            <img src="images/sto.png" alt="">
+            <img src="images/baumit-logo-cover.jpg" alt="">
+            <img src="images/dede1.png" alt="">
+            <img src="images/rigips.png" alt="">
+            <img src="images/milwaukee4555.jpg" alt="">
+        </div>
+    </div>
+    <div class="total">
+        <div class="description-partner">
+            <p>
+                Suntem mândri să colaborăm cu parteneri de încredere precum Dedeman,
+                Baumit, Sto, Rigips și Milwaukee, companii cu reputație solidă în
+                industria construcțiilor. Prin parteneriatul nostru cu Dedeman,
+                beneficiem de acces la o gamă diversificată de materiale de construcții
+                de cea mai înaltă calitate, asigurându-ne că fiecare proiect
+                beneficiază de resurse durabile și fiabile. Colaborarea cu Baumit și
+                Sto aduce în proiectele noastre soluții inovatoare în domeniul
+                izolației termice, finisajelor exterioare și interioare, garantând
+                nu doar eficiență energetică, ci și un design de excepție. Cu Rigips,
+                expert în sisteme de gips-carton, aducem în spațiile noastre confort
+                și flexibilitate în amenajare. Parteneriatul cu Milwaukee, lider în
+                echipamente electrice de construcții, ne oferă acces la tehnologie de
+                vârf pentru a asigura performanță și eficiență în fiecare etapă a
+                construcției. Aceste parteneriate esențiale reflectă angajamentul
+                nostru pentru calitatea și excelența în toate proiectele noastre de
+                construcții.</p>
+        </div>
+    </div>
 </body>
+
 </html>
 <?php include 'footer.php'; ?>
