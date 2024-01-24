4b735aff631420305285e292c41417873748e2ea
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-01-24 18:31:59 +0200

    servici pg

--- portofoliu.php
+++ portofoliu.php
@@ -1,2 +1,17 @@
-<?php include 'header.php'; ?>
-<?php include 'footer.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="portofoliu.css">
+    <title>Portofoliu</title>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
--- servicii.php
+++ servicii.php
@@ -5,10 +5,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Servici</title>
+    <link rel="stylesheet" href="servici.css">
 </head>
 
 <body>
     <?php include 'header.php'; ?>
+    <div class="servici">
+        <h1>Servicii oferite de BRL Construct:</h1>
+        <div class="list-of-services">
+            <div class="card1">
+                <h3>Construcții de Case la Cheie:</h3>
+                <p>Proiectare și construcție de case unifamiliale, adaptate nevoilor și preferințelor clienților.</p>
+                <p>Utilizarea materialelor de înaltă calitate pentru durabilitate și eficiență energetică.
+                </p>
+                <p>Proces simplificat, de la concept până la finalizare, pentru a oferi clienților o experiență fără griji.
+                </p>
+            </div>
+            <div class="card2">
+                <h3>Construcții de Blocuri Rezidențiale:</h3>
+                <p>Dezvoltare și construcție de blocuri rezidențiale moderne și funcționale.</p>
+                <p>Abordare inovatoare în proiectare pentru optimizarea spațiului și confortul locatarilor.
+                </p>
+                <p>Implementare strictă a standardelor de siguranță și eficiență energetică.
+                </p>
+            </div>
+            <div class="card3">
+                <h3>Extinderi și Renovări:
+                </h3>
+                <p>Servicii complete de extindere și renovare a clădirilor existente.
+                </p>
+                <p>Actualizare și modernizare a spațiilor pentru a răspunde nevoilor schimbătoare ale clienților.
+                </p>
+                <p>Integrarea soluțiilor inovatoare pentru eficiență și estetică îmbunătățită.
+                </p>
+            </div>
+            <div class="card4">
+                <h3>Lucrări Comerciale:</h3>
+                <p>Construcții și renovări de spații comerciale și birouri.
+                </p>
+                <p>Adaptarea spațiilor pentru cerințele specifice ale afacerilor și industriei.
+                </p>
+                <p>Punctualitate în livrarea proiectelor pentru minimalizarea perturbărilor în activitățile comerciale.
+                </p>
+            </div>
+
+        </div>
+        <div class="final-text">
+        <div class="text">BRL Construct se dedică oferirii unor servicii complete și de
+            înaltă calitate, acoperind toate aspectele construcțiilor, de la
+            proiectare până la finalizare. Fiecare proiect este tratat cu
+            profesionalism și devotament pentru a asigura rezultatele dorite
+            de clienți.</div></div>
+    </div>
+
+
     <?php include 'footer.php'; ?>
 </body>
 
